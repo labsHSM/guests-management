81a719fe8f20ac3e9e680a3ac1c89d1806404140
sap <sabark@eluocnc.com>
2024-07-15 17:29:46 +0900

    mod: Search -> 검색

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -42,7 +42,7 @@ export default function SearchInput({ className }: SearchInputProps) {
           type="submit"
           className="text-white absolute right-2.5 top-1/2 transform -translate-y-1/2 bg-[#5175FD] hover:bg-indigo-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
-          Search
+          검색
         </button>
       </div>
     </form>
