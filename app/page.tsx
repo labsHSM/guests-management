6c9db0f0c640a357678678386080d1ed05456c3a
mykoo <mykoo@eluocnc.com>
2024-07-16 15:20:43 +0900

    lint test

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
-"use client";
+'use client';
 
-import Link from "next/link";
-import { useRouter } from "next/navigation";
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 
 export default function Home() {
   const router = useRouter();
